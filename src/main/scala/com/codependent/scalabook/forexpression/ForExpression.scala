e9b91a2bd14b528cb6b69ec8a6cc871d96853feb
jainigo <jainigo@profile.es>
2020-10-31 20:07:23 +0100

    for expr

--- src/main/scala/com/codependent/scalabook/forexpression/ForExpression.scala
+++ src/main/scala/com/codependent/scalabook/forexpression/ForExpression.scala
@@ -10,4 +10,8 @@ object ForExpression extends App {
   val doubled2 = for (n <- nums) yield n * 2
   println(doubled2)
 
+  val names = List("_adam", "_david", "_frank")
+  val capitalizedNames = for (name <- names) yield name.drop(1).capitalize
+  println(capitalizedNames)
+
 }
