da5a04ba75aad3224adc40cc7c28be3e22587977
jainigo <jainigo@profile.es>
2020-11-02 23:44:52 +0100

    folding

---
+++ src/main/scala/com/codependent/scalabook/folding/FoldApp.scala
@@ -0,0 +1,31 @@
+package com.codependent.scalabook.folding
+
+import com.codependent.scalabook.clazz.Person
+
+object FoldApp extends App {
+
+  def getFromCache(id: Int): Option[Person] = {
+    if (id % 2 == 0) {
+      Some(Person(id.toString, id.toString))
+    } else {
+      None
+    }
+  }
+
+  def getFromDatabase(id: Int): Option[Person] = {
+    if (id % 3 == 0) {
+      Some(Person(id.toString, id.toString))
+    } else {
+      None
+    }
+  }
+
+  def get(id: Int): Option[Person] = {
+    getFromCache(id).fold(getFromDatabase(id))(identity(Option(_)))
+  }
+
+  println(get(2))
+  println(get(3))
+  println(get(1))
+
+}
